dc4cbafc412f7b4d9f2358e426d922411f224922
Briane Samson <brianesamson@Brianes-MacBook-Pro.local>
2017-12-07 12:13:52 +0900

    Updated ALLOWED_HOSTS again

--- taxidash/settings.py
+++ taxidash/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '+852ojo&6w!&w*ffvv8f+(sdhep1g#
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['https://taxidash.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['taxidash.herokuapp.com','127.0.0.1']
 
 
 # Application definition
